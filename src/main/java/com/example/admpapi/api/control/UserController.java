516353b6195bf88b6b927a1351e164643fe4e27d
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2024-09-04 14:57:54 +0700

    Update Controller User

--- src/main/java/com/example/admpapi/api/control/UserController.java
+++ src/main/java/com/example/admpapi/api/control/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.Optional;
 
-@RestController
+@RestController("api/v1/user")
 public class UserController {
 
     private UserService userService;
