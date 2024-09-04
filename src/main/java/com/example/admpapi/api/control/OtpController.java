7d615cf72a77a84881d7da9eb875398f83d3b15b
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2024-09-04 20:35:37 +0700

    Feature: basic send & verify OTP v2

--- src/main/java/com/example/admpapi/api/control/OtpController.java
+++ src/main/java/com/example/admpapi/api/control/OtpController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/otp")
+@RequestMapping("/api/v1/otp")
 public class OtpController {
 
     private final EmailService emailService;
